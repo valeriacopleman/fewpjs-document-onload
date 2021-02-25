8f5c7518876665e4994635559855c1af5ee5677c
Valeria Copleman <valeriacopleman@gmail.com>
2021-02-25 13:16:23 -0800

    commit tree 08d79c717722e6f56e81aab6b0bb377e93a36a11

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('text').textContent = "This is really cool!";
+    //let text = document.getElementById('divA').textContent;
+    //document.getElementById('divA').textContent = 'This text is different!';
+  });
